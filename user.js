76d91089ee76538a635020efd5410c5fbbc7c51e
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-04-28 16:58:12 +0200

    credo fine

--- user.js
+++ user.js
@@ -27,3 +27,21 @@ const user3 = new User("Marco", "Galli", 28, "Firenze");
 console.log(user1.compareAge(user2));
 console.log(user2.compareAge(user1));
 console.log(user1.compareAge(user3));
+
+class Pet {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
+  condividonoProprietario(otherPet) {
+    return this.ownerName === otherPet.ownerName;
+  }
+}
+
+const pet1 = new Pet("Fido", "Gianfranco", "Cane", "Labrador");
+const pet2 = new Pet("Micio", "Giulia", "Gatto", "Persiano");
+const pet3 = new Pet("Pippo", "Gianfranco", "Cane", "Beagle");
+
+console.log(pet1.condividonoProprietario(pet2));
